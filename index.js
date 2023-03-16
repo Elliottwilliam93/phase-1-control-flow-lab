35d871e9af8603b9456d46604eeace24a7da7139
williamelliott <ironmaiden229@hotmail.com>
2023-03-16 12:12:20 -0400

    project finished

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result;
+  if (feet <= 400) {;
+    result = 'This one is on me!';
+      return result;
+  } else if (feet < 2000) {;
+    result = 'That will be twenty bucks.';
+      return result;
+  } else if (feet < 2500) {;
+    result = 'I will gladly take your thirty bucks.';
+      return result;
+  } else (feet) ;{
+    result = 'No can do.';
+      return result;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result;
+  if (city === 'NYC') {
+    result = 'Ok, sounds good.'
+      return result
+  } else if (city <= 'Pittsburgh') {
+    result = 'No go.'
+      return result
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result;
+  if (tip === 'generous') {
+    result = 'Thank you so much.'
+      return result
+  } else if (tip <= 'not as generous') {
+      result = 'Thank you.'
+        return result
+  } else (tip >= 'Thanks for everything') ;{
+      result = 'Bye.'
+        return result
+  }
 }
